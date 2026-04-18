e456d6f44feb134e893a78c4901d9cba8e19b5c4
LordMidas <55047920+LordMidas@users.noreply.github.com>
2026-04-18 08:31:21 -0400

    fix(vanilla): event screen title not passing through XBBCODE
    Bug report: https://steamcommunity.com/app/365360/discussions/1/800093196998975135/

---
+++ ui/mods/nested_tooltips/js_hooks/screens/world/world_event_screen.js
@@ -0,0 +1,14 @@
+NestedTooltips.Hooks.WorldEventScreen_updateHeader = WorldEventScreen.prototype.updateHeader;
+WorldEventScreen.prototype.updateHeader = function (_data)
+{
+	// VanillaFix: https://steamcommunity.com/app/365360/discussions/1/800093196998975135/
+	// Pass the Title through XBBCODE so that nested tooltips in it are parsed properly.
+	if (WorldEventIdentifier.Event.Title in _data && _data['title'] !== null)
+	{
+		_data.title = XBBCODE.process({
+			text: _data.title,
+		}).html;
+	}
+
+	NestedTooltips.Hooks.WorldEventScreen_updateHeader.call(this, _data);
+}
